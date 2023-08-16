86613091a1f50e15afbf0ac2effac4ef292b0381
Snoopmallymal <mcflood@gmx.com>
2023-08-16 08:29:06 -0500

    Made Top Buttons Function

--- js/buttons.js
+++ js/buttons.js
@@ -5,3 +5,12 @@ function gameFullscreen() {
     // Make It Fullscreen
     frame.requestFullscreen();
 }
+
+function openHome() {
+    window.open("/index.html", "_self", "", "false")
+}
+
+// Opens The News Page
+function openNews() {
+    window.open("path", "_blank", "", "false")
+}
